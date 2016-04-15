04178024aaa0084cd28e7d48d2c3ef169de9231d
Rebecca Hom <rhom0505@gmail.com>
2016-04-15 12:33:33 -0700

    modified index.html

--- src/ui/js/format.js
+++ src/ui/js/format.js
@@ -1,5 +1,18 @@
 /* Custom JavaScript functions */
 
 /* Function for setting up header */
+function header(pageName) {
+    // Insert page name
+    var title = $("<h1></h1>").text(pageName);
+    $(".app-header").append(title);
+}
+
+/* Function for menu */
+
 
 /* Function for setting up footer */
+function footer() {
+    // Get footer information
+    var info = $("<h1></h1>").text("Footer information");
+    $(".app-footer").append(info);
+}
