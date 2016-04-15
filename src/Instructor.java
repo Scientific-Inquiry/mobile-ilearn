55ba3a296ca924f4820412527969f2da6154e6a8
Krissi Mena <kmena003@ucr.edu>
2016-04-15 03:18:06 -0700

    assignments output JSON

---
+++ src/Instructor.java
@@ -0,0 +1,11 @@
+
+public class Instructor extends User{
+    public Instructor()
+    {
+        super();
+    }
+    public Instructor(long u_id)
+    {
+        super(u_id, 1);
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,11 @@
+
+public class Student extends User{
+    public Student()
+    {
+        super();
+    }
+    public Student(long u_id)
+    {
+        super(u_id, 0);
+    }
+}
