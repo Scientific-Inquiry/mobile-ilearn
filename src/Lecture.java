ceaf693a8658f74b78f18d0aa50eca7273ebeb67
xArchange <candice.bentejac@gmail.com>
2016-04-18 18:42:04 -0700

    Added class related files to the master branch.

---
+++ src/Lecture.java
@@ -0,0 +1,29 @@
+public class Lecture extends Class {
+    public Lecture(String number, String section, String name, String quarter, /*ArrayList<String> assignments, ArrayList<String> courseMaterial, ArrayList<String> grades, String syllabus,*/ String faculty)
+    {
+        this.number = new String (number);
+        this.section = new String(section);
+        this.name = new String(name);
+        this.quarter = new String(quarter);
+        /*this.assignments = (ArrayList<String>) assignments.clone();
+        this.courseMaterial = (ArrayList<String>) courseMaterial.clone();
+        this.grades = (ArrayList<String>) grades.clone();
+        this.syllabus = new String (syllabus);*/
+        this.faculty = new String(faculty);
+    }
+
+    public String toString() {
+        return super.toString();
+    }
+
+    public Lecture clone()
+    {
+        return new Lecture(getNumber(), getSection(), getName(), getQuarter(), /*getAssignments(), getCourseMaterial(), getGrades(), getSyllabus(),*/ getFaculty());
+    }
+
+    public static void main(String[] args)
+    {
+        Lecture lec = new Lecture("CS 183", "001", "Intro to Software Engineering", "Sping 2016", "Oben");
+        System.out.println(lec.toString());
+    }
+}
