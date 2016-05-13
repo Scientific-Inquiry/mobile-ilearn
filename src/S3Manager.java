f8def2c92b473e5342ae21866198259e8ad10361
Krissi Mena <kmena003@ucr.edu>
2016-05-13 15:37:29 -0700

    added connection inside of s3

--- src/S3Manager.java
+++ src/S3Manager.java
@@ -1,10 +1,14 @@
 import com.amazonaws.auth.AWSCredentials;
+
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.*;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.io.*;
+import java.sql.DriverManager;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -21,6 +25,10 @@ public class S3Manager {
     public static String bucketName;
     public static String curr_quarter;
     public static String path;
+    private String dbURL = "jdbc:postgresql://dbmilearn.c8o8famsdyyy.us-west-2.rds.amazonaws.com:5432/dbmilearn";
+    private String user = "group5";
+    private String pass = "cs180group5";
+    public Connection connection;
 
     S3Manager(){
         DateFormat dateFormat = new SimpleDateFormat("dyyyy");
@@ -40,7 +48,11 @@ public class S3Manager {
 
         else
             date.setDate(4);
-
+        try {
+            connection = DriverManager.getConnection(dbURL, user, pass);
+        }catch(Exception e){
+            System.out.println("Connection Failed!");
+        }
         bucketName = "milearn";
         credentials = new BasicAWSCredentials("AKIAJWYCYKZJ3BZ5XEBA", "NGJuCS16bH3R6ywlJf7m2NSmdTPd0yA0qANIUDkM");
         s3Client = new AmazonS3Client(credentials);
