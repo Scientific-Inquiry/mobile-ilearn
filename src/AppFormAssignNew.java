bb3de08f0d39df04ae2c832c4e515506d41becc0
xArchange <candice.bentejac@gmail.com>
2016-05-18 07:52:22 +0200

    Update servlet.

--- src/AppFormAssignNew.java
+++ src/AppFormAssignNew.java
@@ -22,7 +22,7 @@ public class AppFormAssignNew extends HttpServlet {
         /* Get the parameters that were given by the user in the form */
         String date = request.getParameter("dueDate");
         System.out.println("Due date: " + date);
-        date = date.substring(0,9) + date.substring(11);
+        date = date.substring(0,9) + " " + date.substring(11);
         System.out.println("Due date: " + date);
 
 
