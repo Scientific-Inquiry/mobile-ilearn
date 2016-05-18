f4974fb0ee4386b159c54b1075c8e475253e7a52
xArchange <candice.bentejac@gmail.com>
2016-05-18 12:59:54 +0200

    Update servlet.

--- src/AppFormAssign.java
+++ src/AppFormAssign.java
@@ -1,12 +1,14 @@
 import org.postgresql.Driver;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -19,20 +21,18 @@ public class AppFormAssign extends HttpServlet {
                           HttpServletResponse response) throws ServletException, IOException {
 
         /* Get the parameters that were given by the user in the form */
-        String assignId = request.getParameter("aid");
+        int assignId = Integer.parseInt(request.getParameter("aid"));
         System.out.println("Assignment ID: " + assignId);
         String title = request.getParameter("title");
         System.out.println("Title: " + title);
         String description = request.getParameter("description");
         System.out.println("Description :" + description);
-        String grade = request.getParameter("grade");
+        Integer grade = Integer.parseInt(request.getParameter("grade"));
         System.out.println("Grade :" + grade);
+        String date = request.getParameter("dueDate");
 
-        /* Check validity of the value that the user has given to the attribute */
-        /*if (theme.length() > 1)
-            theme = "a"; *//* If the parameter is longer than one, then the user has "cheated"
 
-        *//* Register driver (absolutely needed for Tomcat) *//*
+        /* Register driver (absolutely needed for Tomcat) */
         try {
             DriverManager.registerDriver(new Driver());
         }
@@ -44,39 +44,61 @@ public class AppFormAssign extends HttpServlet {
         }
         Connection connection = null;
 
-        *//* Connect to the database that is stored on AWS' RDS *//*
+        /* Connect to the database that is stored on AWS' RDS */
         String dbURL = "jdbc:postgresql://dbmilearn.c8o8famsdyyy.us-west-2.rds.amazonaws.com:5432/dbmilearn";
         String user = "group5";
         String pass = "cs180group5";
 
         try {
-            connection = DriverManager.getConnection(dbURL, user, pass); *//* Now connected! *//*
+            connection = DriverManager.getConnection(dbURL, user, pass); /* Now connected! */
 
-            *//* Select all the existing themes to check that the user didn't cheat and sent the id of
-            a theme that does not exist *//*
-            PreparedStatement st = connection.prepareStatement("SELECT * FROM Theme");
-            ResultSet rs = st.executeQuery();
-            boolean b = false;
-            while (rs.next())
+            if (!title.isEmpty())
             {
-                *//* If the theme given by the user matches one that is in the database, everything
-                is fine *//*
-                if (theme.equals(rs.getString("id")))
-                    b = true;
+                PreparedStatement st = connection.prepareStatement("UPDATE Assignments SET aname = ? WHERE aid = ?");
+                st.setString(1, title);
+                st.setInt(2, assignId);
+                st.executeUpdate();
+                st.close();
             }
-            if (!b) *//* If no theme matched, set the parameter to the default theme *//*
-                theme = "a";
 
-            *//* Process to the database update *//*
-            st = connection.prepareStatement("UPDATE Usr U SET theme = ? WHERE U.unetid = ?;");
-            st.setString(1, theme);
-            st.setString(2, login);
-            st.executeUpdate();
+            if (!description.isEmpty())
+            {
+                PreparedStatement st = connection.prepareStatement("UPDATE Assignments SET description = ? WHERE aid = ?");
+                st.setString(1, description);
+                st.setInt(2, assignId);
+                st.executeUpdate();
+                st.close();
+            }
 
-            rs.close();
-            st.close();
+            if (!date.isEmpty())
+            {
+                Timestamp timestamp = new java.sql.Timestamp(new Date().getTime());
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                Date parsedDate = null;
+                try {
+                    parsedDate = dateFormat.parse(date);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                timestamp = new java.sql.Timestamp(parsedDate.getTime());
+                PreparedStatement st = connection.prepareStatement("UPDATE Assignments SET due = ? WHERE aid = ?");
+                st.setTimestamp(1, timestamp);
+                st.setInt(2, assignId);
+                st.executeUpdate();
+                st.close();
+            }
 
-            *//* Redirect to the static website *//*
+            if (grade != null)
+            {
+                PreparedStatement st = connection.prepareStatement("UPDATE Assignments SET apts = ? WHERE aid = ?");
+                st.setInt(1, grade);
+                st.setInt(2, assignId);
+                st.executeUpdate();
+                st.close();
+            }
+
+
+            /* Redirect to the static website */
             String site = new String("http://milearn.s3-website-us-west-2.amazonaws.com/");
             response.setStatus(response.SC_MOVED_TEMPORARILY);
             response.setHeader("Location", site);
@@ -89,13 +111,13 @@ public class AppFormAssign extends HttpServlet {
 
         finally
         {
-            *//* Close connection with the database *//*
+            /* Close connection with the database */
             try{
                 connection.close();
             } catch (SQLException e) {
                 e.printStackTrace();
             }
-        }*/
+        }
     }
 
 }
--- src/AppFormGradebook.java
+++ src/AppFormGradebook.java
@@ -1,7 +1,6 @@
 import org.postgresql.Driver;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
