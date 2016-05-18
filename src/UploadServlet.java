f9588dd5db4cbaa276a5d925816bc87b56e87888
xArchange <candice.bentejac@gmail.com>
2016-05-18 13:48:20 +0200

    Update servlet.

--- src/UploadServlet.java
+++ src/UploadServlet.java
@@ -18,6 +18,8 @@ public class UploadServlet extends HttpServlet {
 
     private boolean isMultipart;
     private String filePath;
+    private int maxFileSize = 50 * 1024;
+    private int maxMemSize = 4 * 1024;
     private File file ;
 
     public void init( ){
@@ -41,52 +43,58 @@ public class UploadServlet extends HttpServlet {
             out.println("<p>No file uploaded</p>");
             out.println("</body>");
             out.println("</html>");
+            return;
         }
-        else
-        {
-            DiskFileItemFactory factory = new DiskFileItemFactory();
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        // maximum size that will be stored in memory
+        factory.setSizeThreshold(maxMemSize);
+        // Location to save data that is larger than maxMemSize.
+        factory.setRepository(new File("c:\\temp"));
 
-            // Create a new file upload handler
-            ServletFileUpload upload = new ServletFileUpload(factory);
+        // Create a new file upload handler
+        ServletFileUpload upload = new ServletFileUpload(factory);
+        // maximum file size to be uploaded.
+        upload.setSizeMax( maxFileSize );
 
-            try {
-                // Parse the request to get file items.
-                List fileItems = upload.parseRequest(request);
+        try{
+            // Parse the request to get file items.
+            List fileItems = upload.parseRequest(request);
 
-                // Process the uploaded file items
-                Iterator i = fileItems.iterator();
+            // Process the uploaded file items
+            Iterator i = fileItems.iterator();
 
-                out.println("<html>");
-                out.println("<head>");
-                out.println("<title>Servlet upload</title>");
-                out.println("</head>");
-                out.println("<body>");
-                while (i.hasNext()) {
-                    FileItem fi = (FileItem) i.next();
-                    if (!fi.isFormField()) {
-                        // Get the uploaded file parameters
-                        String fieldName = fi.getFieldName();
-                        String fileName = fi.getName();
-                        String contentType = fi.getContentType();
-                        boolean isInMemory = fi.isInMemory();
-                        long sizeInBytes = fi.getSize();
-                        // Write the file
-                        if (fileName.lastIndexOf("\\") >= 0) {
-                            file = new File(filePath +
-                                    fileName.substring(fileName.lastIndexOf("\\")));
-                        } else {
-                            file = new File(filePath +
-                                    fileName.substring(fileName.lastIndexOf("\\") + 1));
-                        }
-                        fi.write(file);
-                        out.println("Uploaded Filename: " + fileName + "<br>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet upload</title>");
+            out.println("</head>");
+            out.println("<body>");
+            while ( i.hasNext () )
+            {
+                FileItem fi = (FileItem)i.next();
+                if ( !fi.isFormField () )
+                {
+                    // Get the uploaded file parameters
+                    String fieldName = fi.getFieldName();
+                    String fileName = fi.getName();
+                    String contentType = fi.getContentType();
+                    boolean isInMemory = fi.isInMemory();
+                    long sizeInBytes = fi.getSize();
+                    // Write the file
+                    if( fileName.lastIndexOf("\\") >= 0 ){
+                        file = new File( filePath +
+                                fileName.substring( fileName.lastIndexOf("\\"))) ;
+                    }else{
+                        file = new File( filePath +
+                                fileName.substring(fileName.lastIndexOf("\\")+1)) ;
                     }
+                    fi.write( file ) ;
+                    out.println("Uploaded Filename: " + fileName + "<br>");
                 }
-                out.println("</body>");
-                out.println("</html>");
-            } catch (Exception ex) {
-                System.out.println(ex);
             }
+            out.println("</body>");
+            out.println("</html>");
+        }catch(Exception ex) {
+            System.out.println(ex);
         }
     }
     public void doGet(HttpServletRequest request,
