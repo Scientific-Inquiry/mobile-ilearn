3d38961f0393ef404791a8b82a26e2eacccd6104
xArchange <candice.bentejac@gmail.com>
2016-05-18 10:15:13 +0200

    Update servlet.

--- src/AppFormAssignNew.java
+++ src/AppFormAssignNew.java
@@ -1,11 +1,11 @@
 import org.postgresql.Driver;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 
@@ -21,14 +21,29 @@ public class AppFormAssignNew extends HttpServlet {
 
         /* Get the parameters that were given by the user in the form */
         String date = request.getParameter("dueDate");
-        date = date.substring(0,10) + " " + date.substring(11) + ":00";
+        if (date.isEmpty()) {
+            int tmp = Integer.parseInt(new Timestamp(new java.util.Date().getTime()).toString());
+            tmp += 86400;
+            date = Integer.toString(tmp);
+        }
+        else
+            date = date.substring(0,10) + " " + date.substring(11) + ":00";
         System.out.println("Due date: " + date);
         String title = request.getParameter("title");
+        if (title.isEmpty())
+            title = "Assignment";
         System.out.println("Title: " + title);
         String description = request.getParameter("description");
+        if (description.isEmpty())
+            description = "There is no description for this assignment.";
         System.out.println("Description: " + description);
-        int grade = Integer.parseInt(request.getParameter("grade"));
+        Integer g = Integer.parseInt(request.getParameter("grade"));
+        if (g == null)
+            g = 10;
+        int grade = g;
         System.out.println("Grade: " + grade);
+        String login = request.getParameter("slogin");
+        System.out.println("Login: " + login);
 
         /* Register driver (absolutely needed for Tomcat) */
         try
@@ -53,7 +68,28 @@ public class AppFormAssignNew extends HttpServlet {
 
             /* Select all the existing themes to check that the user didn't cheat and sent the id of
             a theme that does not exist */
-            PreparedStatement st = connection.prepareStatement("SELECT * FROM Theme");
+            PreparedStatement st = connection.prepareStatement("SELECT T.cid, C.csection, C.cnum FROM teaches T, Usr U, Class C WHERE U.unetid = ? AND T.uid = U.uid");
+            st.setString(1, login);
+            ResultSet rs = st.executeQuery();
+            int idClass = 0;
+            while (rs.next())
+            {
+                String tmp = rs.getString("cnum") + "-" + rs.getString("csection");
+                if (tmp.equals(request.getParameter("tmp"))) {
+                    idClass = rs.getInt("cid");
+                    break;
+                }
+            }
+
+            rs.close();
+
+            st = connection.prepareStatement("INSERT INTO Assignments(cid, aname, description, due, apts) VALUES (?, ?, ?, ?, ?)");
+            st.setInt(1, idClass);
+            st.setString(2, title);
+            st.setString(3, description);
+            st.setString(4, date);
+            st.setInt(5, grade);
+            st.executeUpdate();
 
             st.close();
 
